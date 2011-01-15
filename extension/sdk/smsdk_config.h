0003562f918175b22f114f7dcf1665dcf10ecae1
Asher Baker <asherkin@gmail.com>
2011-01-16 04:37:59 +0700

    Forgot to bump version number, as normal.

--- extension/sdk/smsdk_config.h
+++ extension/sdk/smsdk_config.h
@@ -38,7 +38,7 @@
 /* Basic information exposed publicly */
 #define SMEXT_CONF_NAME			"SteamTools"
 #define SMEXT_CONF_DESCRIPTION	"SteamWorks for SourceMod."
-#define SMEXT_CONF_VERSION		"0.5.2"
+#define SMEXT_CONF_VERSION		"0.5.3"
 #define SMEXT_CONF_AUTHOR		"Asher Baker (asherkin)"
 #define SMEXT_CONF_URL			"http://limetech.org/"
 #define SMEXT_CONF_LOGTAG		"STEAMTOOLS"
