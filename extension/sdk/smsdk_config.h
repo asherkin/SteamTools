8b70d925f01b7c3e6e517e579b19b1d6e5935851
Asher Baker <asherkin@gmail.com>
2014-02-20 15:53:32 +0000

    Ported to AMBuild.

--- extension/sdk/smsdk_config.h
+++ extension/sdk/smsdk_config.h
@@ -38,8 +38,8 @@
 /* Basic information exposed publicly */
 #define SMEXT_CONF_NAME			"SteamTools"
 #define SMEXT_CONF_DESCRIPTION	"SteamWorks for SourceMod."
-#define SMEXT_CONF_VERSION		"0.8.3"
-#define SMEXT_CONF_AUTHOR		"Asher Baker (asherkin)"
+#define SMEXT_CONF_VERSION		"0.9.0"
+#define SMEXT_CONF_AUTHOR		"Asher \"asherkin\" Baker"
 #define SMEXT_CONF_URL			"http://limetech.org/"
 #define SMEXT_CONF_LOGTAG		"STEAMTOOLS"
 #define SMEXT_CONF_LICENSE		"GPLv3"
